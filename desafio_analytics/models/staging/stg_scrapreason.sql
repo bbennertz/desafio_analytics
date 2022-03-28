3051f402a2e5b4393bcc951630e870c4be3dbaed
Bruno Albino Bennertz <brunobennertz@gmail.com>
2022-03-28 19:07:56 -0300

    stg_scrapreason adicionando

---
+++ desafio_analytics/models/staging/stg_scrapreason.sql
@@ -0,0 +1,9 @@
+with
+    source as(
+        select 
+        scrapreasonid as id_razãosucateamento
+        , name as motivo_sucateamento
+        , modifieddate as data_modificação
+        from {{source('analytics','raw_scrapreason')}}
+    )
+    select * from source
