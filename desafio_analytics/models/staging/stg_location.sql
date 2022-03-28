443a3a1564db1a6594514d35f6ca3d2c92528de7
Bruno Albino Bennertz <brunobennertz@gmail.com>
2022-03-28 18:45:07 -0300

    git teste adicionando stg_location

---
+++ desafio_analytics/models/staging/stg_location.sql
@@ -0,0 +1,11 @@
+with
+    source as(
+        select 
+        locationid as id_localização
+        , costrate as custo_hora
+        , name as nome_local
+        , availability as capacidade_manufatura
+        , modifieddate as data_modificação
+        from {{source('analytics','raw_location')}}
+    )
+    select * from source
