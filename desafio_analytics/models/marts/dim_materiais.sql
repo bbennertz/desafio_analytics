f0c761bfd51a5d910ed85c9c071e31bda7b94e1d
Jessica Oliveira <jesslira23@gmail.com>
2022-03-30 23:34:30 -0300

    adicao de marts e tests

--- desafio_analytics/models/marts/dim_materiais.sql
+++ desafio_analytics/models/marts/dim_materiais.sql
@@ -1,7 +1,36 @@
 with
-    dim_materiais as (
+    stg_materiais as (
         select *
         from {{ ref('stg_billofmaterials') }}
     )
-select *
-from dim_materiais
+    , stg_materiais_with_sk as (
+        select 
+            {{ 
+                dbt_utils.surrogate_key(['id_material']) 
+            }} as sk_material
+            , {{ 
+                dbt_utils.surrogate_key(['id_produto_pai']) 
+            }} as sk_produto_pai
+            , {{ 
+                dbt_utils.surrogate_key(['id_componente']) 
+            }} as sk_componente
+            , *
+        from stg_materiais
+    )   
+    , transformed as (
+        select
+            sk_material
+            , sk_produto_pai
+            , sk_componente
+            , bomlevel
+            , qtd_por_pontagem
+            , data_inicio
+            , data_fim
+            , data_modificada
+        from stg_materiais_with_sk
+        left join {{ ref('dim_produtos')}} 
+            on stg_materiais_with_sk.sk_produto_pai = dim_produtos.sk_produto
+            and stg_materiais_with_sk.sk_componente = dim_produtos.sk_produto
+    )
+select * 
+from transformed
--- desafio_analytics/models/staging/stg_workorderrouting.sql
+++ desafio_analytics/models/staging/stg_workorderrouting.sql
@@ -5,17 +5,17 @@ with
     )
     , transformed as (
         select
-            workorderid as id_fabricacao
-            , productid as id_produto
-            , locationid as id_localizacao
-            , operationsequence as sequencia_operacao
-            , actualresourcehrs as horas_fabricacao
-            , plannedcost as custo_estimado
-            , actualcost as custo_real
-            , scheduledstartdate as data_planejada_inicio_fabricacao
-            , scheduledenddate as data_planejada_fim_fabricacao
-            , actualstartdate as data_inicio_real
-            , actualenddate as data_fim_real
+            "workorderid" as id_ordem_servico
+            , "productid" as id_produto
+            , "locationid" as id_localizacao
+            , "operationsequence" as sequencia_operacao
+            , "actualresourcehrs" as horas_fabricacao
+            , "plannedcost" as custo_estimado
+            , "actualcost" as custo_real
+            , cast(scheduledstartdate as date) as data_planejada_inicio_fabricacao
+            , cast(scheduledenddate as date) as data_planejada_fim_fabricacao
+            , cast(actualstartdate as date) as data_inicio_real
+            , cast(actualenddate as date) as data_fim_real
         from source
     )
 select *
